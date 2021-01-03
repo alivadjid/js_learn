8b3c3594ed8016c822be9cc43973657c3117b32f
alivadjid1 <alivadjid@gmail.com>
2021-01-03 22:05:33 +0300

    Добавлены стили для всех элементов

--- src/model.js
+++ src/model.js
@@ -1,12 +1,15 @@
 import image from './assets/image.png' 
  
+const text = `
+	Крутые уроки
+`
+
  export const model = [
 	{
 		type: 'title',
 		value: 'Конструктор сайтов на чистом Javascript',
 		options: {
 			tag: 'h2',
-			// styles: `background: linear-gradient(to right, #ff0099, #493240); color: #fff; text-align: center; padding: 1.5rem;`
 			styles: {
 				background: 'linear-gradient(to right, #ff0099, #493240)',
 				color: '#fff',
@@ -17,19 +20,41 @@ import image from './assets/image.png'
 	},
 	{
 		type: 'text',
-		value: 'here we go with some text'
+		value: text,
+		options: {
+			styles: {
+				background: 'linear-gradient(to left, #f2994a, #f2c94c)',
+				padding: '1rem',
+				'font-weight':'bolc'
+			}
+		}
 	},
 	{
 		type: 'columns',
 		value: [
-			'1111111',
-			'2222222',
-			'3333333',
+			'Приложение на чистом JS, без использования библиотек',
+			'Узнаешь как работают принципы SOLID и ООП в JS за один курс',
+			'JS - это просто, интересно. Научись создавать любые UI своими руками',
 			'4444444'
-		]
+		],
+		options: {
+			styles: {
+				background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
+				padding: '2rem',
+				color: '#fff',
+				'font-weight':'bold'
+			}
+		}
 	},
 	{
 		type: 'image',
-		value: image
+		value: image,
+		options: {
+			styles: {
+				padding: '2rem 0',
+				display: 'flex',
+				'justify-content': 'center'
+			}
+		}
 	}
 ]
